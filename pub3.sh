5cb7830e2b5e3390c1878de94a792651f18105ec
Eric Haas <ehaas@healthedatainc.com>
2018-02-12 12:30:53 -0800

    combined bash scripts into one

--- pub3.sh
+++ pub3.sh
@@ -3,26 +3,38 @@ name="IG-Template2"
 # $PWD=/Users/ehaas/Documents/FHIR/IG-Template2
 path2=/Users/ehaas/Downloads/
 path3=/Users/ehaas/Documents/FHIR/IG-tools/
+while getopts ds:t option
+do
+ case "${option}"
+ in
+ d) DEFN=1;;
+ s) SOURCE=${OPTARG};;
+ t) NA='N/A';;
+ esac
+done
 echo "================================================================="
 echo === Publish $name IG!!! $(date -u) ===
 echo see 'local workflow.md' file for how to use
-echo "Positional Parameters"
-echo 'source directory = ' $1
-echo 'N/A for no terminology server (run faster and offline)= ' $2
-echo '$3 = ' $3
-echo === create ig.json and ig.xml in $PWD and ../$1 ===
+echo "Optional Parameters"
+echo '-d parameter = create definitions files  = ' $DEFN
+echo '-s parameter = source directory = ' $SOURCE
+echo '-t parameter for no terminology server (run faster and offline)= ' $NA
+echo === create ig.json and ig.xml in $PWD and ../$SOURCE ===
 echo "================================================================="
 echo getting rid of .DS_Store files since they gum up the igpublisher....
 find . -name '.DS_Store' -type f -delete
 sleep 1
 # git status
-echo "================================================================="
-echo === run definitions maker with optional source directory name as first argument ===
-echo "================================================================="
-python3.5 ${path3}definitions.py $1
-sleep 1
-git status
+if [[ $DEFN ]]; then
+  echo "================================================================="
+  echo === run definitions maker with optional source directory name as first argument ===
+  echo "================================================================="
+  python3.5 ${path3}definitions.py $SOURCE
+  sleep 1
+  git status
+
+fi
 echo "================================================================="
 echo === run igpublisher ===
 echo "================================================================="
-java -jar ${path2}org.hl7.fhir.igpublisher.jar -ig ig.json -watch -tx $2
+java -jar ${path2}org.hl7.fhir.igpublisher.jar -ig ig.json -watch -tx $NA
