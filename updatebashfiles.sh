d871aada6779ce18e645ad7028c12e94b7d3c626
Healthedata1 <ehaas@healthedatainc.com>
2018-10-24 22:38:37 -0700

    add bash file update

--- updatebashfiles.sh
+++ updatebashfiles.sh
@@ -15,7 +15,7 @@ path2=/Users/ehaas/Documents/FHIR/Davinci-DEQM
 # path2=/Users/ehaas/Documents/FHIR/Argo-ClinicalNotes
 # path2=/Users/ehaas/Documents/FHIR/Argo-Questionnaire
 
-filelist="source/pages/html_pages.html source/pages/toc.md framework/_includes/toc-generator.md source/pages/all-examples.md framework/_includes/example-list-generator.md framework/_config.yml framework/_includes/breadcrumbs.html framework/_includes/pagenumber.html framework/_includes/publish-box.html framework/_includes/footer.html framework/_layouts/default.html framework/_layouts/fhir-artifact.html framework/assets/css/project.css sd.html"
+filelist="pub3.sh push.sh"
 for f in ${filelist}; do
 echo copy ${path1}/${f} to ${path2}/${f}
 cp ${path1}/${f} ${path2}/${f}
