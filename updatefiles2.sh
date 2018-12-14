4555976a2832997b832cc06af05fe6b792f5d6db
Healthedata1 <ehaas@healthedatainc.com>
2018-12-13 17:11:42 -0800

    updates to Any.html and footer.html

--- updatefiles2.sh
+++ updatefiles2.sh
@@ -6,7 +6,7 @@ name="IG-Template"
 path1=/Users/ehaas/Documents/FHIR/IG-Template2
 # pathlist="/Users/ehaas/Documents/FHIR/Argo-ClinicalNotes"
 pathlist="/Users/ehaas/Documents/FHIR/IG-Template2/newfolder /Users/ehaas/Documents/FHIR/IG-Test3 /Users/ehaas/Documents/FHIR/IG-Sample /Users/ehaas/Documents/FHIR/US-Core-R4 /Users/ehaas/Documents/FHIR/Davinci-DEQM /Users/ehaas/Documents/FHIR/Davinci-DEQM-r4 /Users/ehaas/Documents/FHIR/VhDir /Users/ehaas/Documents/FHIR/OMHtoFHIR /Users/ehaas/Documents/FHIR/Argo-ClinicalNotes /Users/ehaas/Documents/FHIR/Argo-Questionnaire"
-filelist="framework/assets/css/project.css"
+filelist="framework/_includes/footer.html"
 #filelist="source/pages/html_pages.html source/pages/toc.md framework/_includes/toc-generator.md source/pages/all-examples.md framework/_includes/example-list-generator.md framework/_config.yml framework/_includes/breadcrumbs.html framework/_includes/pagenumber.html framework/_includes/publish-box.html framework/_includes/footer.html framework/_layouts/default.html framework/_layouts/fhir-artifact.html framework/assets/css/project.css sd.html pub3.sh push.sh source/pages/downloads.md framework/_includes/schematron-list-generator.md framework/_includes/examplebutton.html framework/_includes/header.html"
 for p in ${pathlist}; do
 for f in ${filelist}; do
